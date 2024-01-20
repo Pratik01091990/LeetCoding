cbf159394038d805a3d1cf2d1b2cb3e7d0c94b5d
Pratik Pal <40118356+Pratik01091990@users.noreply.github.com>
2024-01-20 20:21:59 +0530

    Time: 130 ms (92.46%) | Memory: 18.2 MB (52.47%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -7,14 +7,14 @@ class Solution(object):
         element = set (nums)
         mx_count = 0
         mx_element = 0
-
+        ml = len(nums)/2 
         for i in element:
             cnt = nums.count(i)
             if mx_count <= cnt:
                 mx_count = cnt
                 mx_element = i
             
-            if mx_count > len(nums)/2:
+            if mx_count > ml:
                 break
         
         return mx_element
