403e41279d0a1fac545db96b7c726abb096a4a16
Pratik Pal <40118356+Pratik01091990@users.noreply.github.com>
2024-02-01 00:25:22 +0530

    Time: 37 ms (56.34%) | Memory: 16.6 MB (66.43%) - LeetSync

--- 290-word-pattern/word-pattern.py
+++ 290-word-pattern/word-pattern.py
@@ -2,21 +2,24 @@ class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         wordlst = s.split()
         pat = {}
-        #pat2 = {}
-        # check the lenghts
+
+        # check the lenghts if not same then return fasle
         if (len(pattern) != len(wordlst)):
             return False
-        
+
+        # check the character is in the pattern and the word is mapped to the pattern        
         for h,i in enumerate(pattern):
-            #print(pat)
+            # if the character and pattern not in the dictionary then map the word
             if (i not in pat.keys()) and (wordlst[h] not in pat.values()):
                 pat[i] = wordlst[h]
+            # if the character is present and mapped to the correct word then continue checking
             elif(i in pat.keys()) and (wordlst[h] == pat[i]):
                 continue
+            # if the above conditions fail then the character is in the dictionary and mapped to a different word 
             else:
                 return False
 
-        
+        # if the control flow is here then retunrn true
         return True
 
 
