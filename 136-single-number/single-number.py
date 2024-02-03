0c131c82b1c75ee537c2aa65c8bdac7c5505ace2
Pratik Pal <40118356+Pratik01091990@users.noreply.github.com>
2024-02-03 13:54:21 +0530

    Time: 108 ms (83.45%) | Memory: 19.1 MB (66.73%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,16 +1,21 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+
+        #sprt the array so that the duplicates are together
         nums.sort()
-        print(nums)
+        
+        # Initite the window pair
         l = 0
         r = 1
         while r < len(nums):
+            #check for each pair if not unique then shift the window
             if nums[l] == nums[r]:
                 l += 2
                 r += 2
             else:
                 return nums[l]
 
+        # if we dont find the unique the the last number is unique 
         return nums[len(nums) -1]
 
         
